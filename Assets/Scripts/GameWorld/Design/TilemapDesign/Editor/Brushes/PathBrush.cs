162bf7c7838c9061f815f6c0dbec30c6982e1863
Aykut Aslan <aslanaykut4@gmail.com>
2022-06-26 10:32:27 +0300

    Create PathBrush (Only Inheriting TintBrush for now)

---
+++ Assets/Scripts/GameWorld/Design/TilemapDesign/Editor/Brushes/PathBrush.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace ICouldGames.DefenseOfThrones.GameWorld.Design.TilemapDesign.Editor.Brushes
+{
+    [CustomGridBrush(false, false, false, "Path Brush")]
+    public class PathBrush : TintBrush
+    {
+
+    }
+}
---
+++ Assets/Scripts/GameWorld/Design/TilemapDesign/Editor/Brushes/PathBrushEditorcs.cs
@@ -0,0 +1,11 @@
+﻿using UnityEditor;
+using UnityEditor.Tilemaps;
+
+namespace ICouldGames.DefenseOfThrones.GameWorld.Design.TilemapDesign.Editor.Brushes
+{
+    [CustomEditor(typeof(PathBrush))]
+    public class PathBrushEditor : TintBrushEditor
+    {
+
+    }
+}
