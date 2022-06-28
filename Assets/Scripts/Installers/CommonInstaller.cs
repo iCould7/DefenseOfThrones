b9db89368ce8ff92b965894c9b7a0eef2e715f77
Aykut Aslan <aslanaykut4@gmail.com>
2022-06-28 07:40:22 +0300

    Setup SceneContext

---
+++ Assets/Scripts/Installers/CommonInstaller.cs
@@ -0,0 +1,12 @@
+﻿using Zenject;
+
+namespace ICouldGames.DefenseOfThrones.Installers
+{
+    public class CommonInstaller : Installer<CommonInstaller>
+    {
+        public override void InstallBindings()
+        {
+            SignalBusInstaller.Install(Container);
+        }
+    }
+}
---
+++ Assets/Scripts/Installers/GameSceneInstaller.cs
@@ -0,0 +1,14 @@
+﻿using Zenject;
+
+namespace ICouldGames.DefenseOfThrones.Installers
+{
+    public class GameSceneInstaller : MonoInstaller<GameSceneInstaller>
+    {
+        public override void InstallBindings()
+        {
+            CommonInstaller.Install(Container);
+
+
+        }
+    }
+}
